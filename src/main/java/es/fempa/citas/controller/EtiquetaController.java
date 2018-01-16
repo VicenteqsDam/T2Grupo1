435bdbe0d1bbd62c2f642ec5822706819a5cfac0
Javier del Rey <javidelrey3@gmail.com>
2018-01-16 20:51:09 +0100

    pruebaCambios Miguel

--- src/main/java/es/fempa/citas/controller/EtiquetaController.java
+++ src/main/java/es/fempa/citas/controller/EtiquetaController.java
@@ -1,5 +1,18 @@
 package es.fempa.citas.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.fempa.citas.domain.Etiqueta;
+
+@RestController
 public class EtiquetaController {
+
+	@PostMapping("/crearEtiqueta")
+	public Etiqueta crearEtiqueta() {
+
+		Etiqueta etiqueta = new Etiqueta();
+		return etiqueta;
+	}
 
 }
