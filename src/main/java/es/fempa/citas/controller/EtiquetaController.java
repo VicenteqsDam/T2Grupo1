4f4342502cad0ad86bbeebd0bedbd83ca516f86f
Unknown <javidelrey3@gmail.com>
2018-01-16 20:59:00 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/es/fempa/citas/controller/EtiquetaController.java
+++ src/main/java/es/fempa/citas/controller/EtiquetaController.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+<<<<<<< Updated upstream
 import org.springframework.web.bind.annotation.RestController;
 
 import es.fempa.citas.domain.Etiqueta;
+=======
+
+import es.fempa.citas.domain.Etiqueta;
+
+>>>>>>> Stashed changes
 public class EtiquetaController {
 
 	@PostMapping("/crearEtiqueta")
@@ -16,10 +22,9 @@ public class EtiquetaController {
 		return etiqueta;
 	}
 
-	
 	@GetMapping("/listadoEtiquetas")
-	public List<Etiqueta> listadoEtiquetas(){
-		
+	public List<Etiqueta> listadoEtiquetas() {
+
 		return null;
 	}
 }
